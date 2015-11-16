3fb1d771c24106419cd579e6974b66341764591a
shuvrow <shuvrow@gmail.com>
2015-11-15 21:57:08 -0500

    Active User validation

--- app/Http/Middleware/AuthenticUser.php
+++ app/Http/Middleware/AuthenticUser.php
@@ -23,7 +23,7 @@ class AuthenticUser extends  Authenticate
     {
 
 
-        $active_user=Users::where('email',$request->input('email'))->first();
+        $active_user=Users::where('email',$request->input('email'))->where('status','=',1)->first();
         if(Hash::check($request->input('password'),$active_user['password']))
         {
             Session::put('user_active',1);
@@ -36,7 +36,7 @@ class AuthenticUser extends  Authenticate
         }
         else
         {
-            return view('registration/login')->with(array('msg'=>'Wrong Credentials'));
+            return view('registration/login')->with(array('msg'=>'Wrong Credentials or User  inactive'));
         }
 
     }
